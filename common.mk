9ecbfabd94a2beb877f25082754247ae642df126
Alexander Koskovich <zvnexus@outlook.com>
2021-07-14 20:14:29 -0700

    sm8150-common: Import CarrierConfigResCommon QSSI overlays
     * from LA.QSSI.11.0.r1-12400-qssi.0.
    
    Change-Id: I1b94e4b29c07f9b186fb10470d2fd083667dc7fc
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- common.mk
+++ common.mk
@@ -520,6 +520,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    CarrierConfigResCommon \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
