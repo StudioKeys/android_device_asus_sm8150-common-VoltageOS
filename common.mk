779faf662d68e82013e811437da2e26b1c1243ed
Jackeagle <jackeagle102@gmail.com>
2022-07-01 00:53:06 -0400

    sm8150-common: Build touch gesture hal
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- common.mk
+++ common.mk
@@ -523,6 +523,10 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.0.vendor
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.asus_msmnile
+
 # TouchGestures
 PRODUCT_PACKAGES += \
     TouchGestures
